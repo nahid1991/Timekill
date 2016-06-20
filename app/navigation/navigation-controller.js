0d87c978e50898198aa73fcec613c481275bfb54
Nahid Shaiket <nahidshaiket10300@gmail.com>
2016-06-20 23:47:38 +0600

    slight fix

--- app/navigation/navigation-controller.js
+++ app/navigation/navigation-controller.js
@@ -3,7 +3,7 @@
       $state, $http){
         $scope.logUserIn = function(){
             $http.post('api/user/login', $scope.login).success(function(response){
-                localStorage.setItem('User-Datat', JSON.stringify(response));
+                localStorage.setItem('User-Data', JSON.stringify(response));
             }).error(function(error){
                 console.log(error);
             });
